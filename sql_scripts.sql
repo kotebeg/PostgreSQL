91e821d34b639ccd2ca03d271917d851e747e093
kotebeg <kotebeg@gmail.com>
2024-09-25 23:37:09 +0400

    added new features

--- sql_scripts.sql
+++ sql_scripts.sql
@@ -6,3 +6,9 @@ SELECT (2);
 SELECT (2.0);
 SELECT (2.0::INTEGER);
 SELECT (2.0::SMALLINT);
+SELECT (999999999::SMALLINT);
+select (1.99999::decimal - 1.99998::decimal);
+
+-- select (1.99999::decimal - 1.99998::decimal);
+select(1.4::integer);   -- rounds numbers
+select(1.7::integer);   -- rounds numbers
