4ba9c0c37541277acfc9eb66a4bb8dd06c0ccc58
kotebeg <kotebeg@gmail.com>
2024-09-28 15:47:06 +0400

    three table joins

--- join_types.sql
+++ join_types.sql
@@ -32,35 +32,8 @@ from photos
 full join users on users.id = photos.user_id
 
 
--- Inner Join:
--- List all photos along with the username of the user who posted each photo.
-
--- Inner Join:
--- List all photos along with the username of the user who posted each photo.
-
-SELECT photos.url, users.username
-FROM photos INNER JOIN users ON photos.user_id = users.id;
 
 
--- Left Join:
--- List all users and their photos, including users who haven't posted any photos.
-select  users.username, photos.url
-from users left join  photos on users.id = photos.user_id; 
-
--- Right Join:
--- List all photos and their associated users, including photos that have no associated user (e.g., NULL user_id).
-
-SELECT photos.url, users.username from users right join  photos on users.id = photos.user_id;
-
-
--- Full Join:
--- List all users and photos, including users without photos and photos without associated users.
-
-SELECT users.username, photos.url  from users full join  photos on users.id = photos.user_id;
-
-
--- Inner Join with Multiple Tables:
--- List all comments with the photo URL and the username of the user who posted the comment..
 
 
 -- 57. Where with Join
@@ -80,3 +53,41 @@ select photos.id, photos.url,  comments.id, comments.contents , users.id, users.
 from comments
 join photos on photos.id = comments.photo_id
 join users on users.id = comments.photo_id and users.id = photos.user_id;
+
+
+-- practice ------------###################################################
+
+-- 1. Inner Join:
+-- List all photos along with the username of the user who posted each photo.
+
+SELECT photos.url, users.username
+FROM photos INNER JOIN users ON photos.user_id = users.id;
+
+
+-- 2. Left Join:
+-- List all users and their photos, including users who haven't posted any photos.
+select  users.username, photos.url
+from users left join  photos on users.id = photos.user_id; 
+
+-- 3. Right Join:
+-- List all photos and their associated users, including photos that have no associated user (e.g., NULL user_id).
+
+SELECT photos.url, users.username from users right join  photos on users.id = photos.user_id;
+
+
+-- 4. Full Join:
+-- List all users and photos, including users without photos and photos without associated users.
+
+SELECT users.username, photos.url  from users full join  photos on users.id = photos.user_id;
+
+
+--  5. Inner Join with Multiple Tables:
+-- List all comments with the photo URL and the username of the user who posted the comment..
+
+select comments.contents, photos.url, users.username
+from comments
+join photos on photos.id = comments.photo_id
+join users on photos.user_id = users.id
+
+-- 6. Left Join with WHERE:
+-- List all users who have posted a comment, along with the comment contents.
